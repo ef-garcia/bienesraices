7d9b40db87c579def6cdf0503baa97a9099856d9
Fabian <garciafabianemmanuel@gmail.com>
2025-06-14 16:06:07 -0300

    nodemailer

--- src/schemas/usuario.schema.js
+++ src/schemas/usuario.schema.js
@@ -1,21 +1,6 @@
-// import { body } from "express-validator";
 import { check, validationResult } from 'express-validator'
 
 
-// const usuarioSchema = [
-//     body("nombre")
-//         .exists().withMessage("El nombre es requerido")
-//         .isString().withMessage("El nombre debe ser un string")
-//         .isLength({ min: 4 }).withMessage("El nombre debe tener al menos 3 caracteres"),
-//     body("email")
-//         .exists().withMessage("El email es requerido")
-//         .isEmail().withMessage("Debe ser un email"),
-//     body("password")
-//         .exists().withMessage("La contraseña es requerida")
-//         .isLength({ min: 6 }).withMessage("La contraseña debe tener al menos 6 caracteres"),
-//     body("repetir_password")
-//         .equals("password").withMessage("La contraseñas no son iguales")
-// ]
 
 const usuarioSchema = async(req, res) =>  {
 
