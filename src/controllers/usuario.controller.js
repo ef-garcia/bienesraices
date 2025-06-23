42843c25a020d529164bf4d04d21256325c56d2b
Fabian <garciafabianemmanuel@gmail.com>
2025-06-23 01:01:39 -0300

    confirmar si el usuario existe y confirmo su cuenta

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -33,6 +33,29 @@ const autenticar = async (req, res) => {
         })
     }
 
+    const { email, password } = req.body;
+    // Comprobar si el usuario existe
+    const usuario = await Usuario.findOne({ where: { email } });
+
+    if(!usuario) {
+        return res.render('auth/login', {
+            pagina: 'Iniciar Sesion',
+            csrfToken: req.csrfToken(),
+            errores: [{ msg: 'El usuario no existe' }]
+        })
+    }
+
+    // Comprobar si el usuario esta confirmado
+    if( !usuario.confirmado ) {
+        return res.render('auth/login', {
+            pagina: 'Iniciar Sesion',
+            csrfToken: req.csrfToken(),
+            errores: [{ msg: 'Tu cuenta no ha sido confirmada' }]
+        })
+    }
+
+    // Revisar el password
+
 }
 
 const formularioRegistro = (req, res) => {    
